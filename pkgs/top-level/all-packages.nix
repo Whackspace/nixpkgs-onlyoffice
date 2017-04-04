fff05a0b73eb58099c3f8fa4f9f382c5adf44cfa
Jörg Thalheim <joerg@thalheim.io>
2017-04-04 22:24:38 +0200

    dracut: init at 044

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -1326,6 +1326,8 @@ in
 
   dtools = callPackage ../development/tools/dtools { };
 
+  dracut = callPackage ../os-specific/linux/dracut { };
+
   dtrx = callPackage ../tools/compression/dtrx { };
 
   dune = callPackage ../development/tools/ocaml/dune { };
